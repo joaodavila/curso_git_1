d43e75dd90d901c4bd982756c77edaf3f2088542
joaodavila <davila.joao@gmail.com>
2023-09-28 08:21:37 -0300

    Testando um commit com mais de uma linha  - serão inseridos dois novos arquivos:  - a.js  - b.js

---
+++ a.js
@@ -0,0 +1,3 @@
+function a(teste){
+    console.log(teste);
+}
---
+++ b.js
@@ -0,0 +1,8 @@
+console.log('xyz');
+
+console.log('finalizando esta parte');
+
+function addCart(produto){
+    cart = {}
+    return cart;
+}
